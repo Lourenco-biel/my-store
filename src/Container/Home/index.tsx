63dfde6109885ed297268933395b3c04d579feb0
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-07-04 14:29:55 -0300

    Criando estilos globais e iniciando estilização da home

---
+++ src/Container/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as C from './style'
+
+function Home(){
+    return (
+        <C.Container>
+            Ola ss
+        </C.Container>
+    );
+}
+
+export default Home;
