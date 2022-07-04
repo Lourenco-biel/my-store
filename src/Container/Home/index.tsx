b3887b2e1a4203c1595eacb9dde014a742d6a218
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-07-04 15:33:16 -0300

    application in the home of registration of name and saving completed

--- src/Container/Home/index.tsx
+++ src/Container/Home/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as C from './style';
 
 function Home(){
+  const [userName, setUserName] = useState<string>('')
+
+  const userRegister = (e: any) => {
+    e.preventDefault()
+    if(userName === ''){
+      alert('Preencha o nome')
+    }else{
+      localStorage.setItem('userName', userName)
+    }
+    
+  }
+
     return (
         <C.Container>
         <C.ContainerItens>
           <h1>Login</h1>
           <label ><i>Nome:</i>
-            <input type='text'  name='name' placeholder='Escreva seu Nome Completo' required />
-            <button type='submit' >Ir para cadastro</button>
+            <input type='text' onChange={(e)=> setUserName(e.target.value)} name='name' placeholder='Escreva seu Nome Completo' required />
+            <button type='submit' onClick={(e) => userRegister(e)} >Ir para cadastro</button>
           </label>
         </C.ContainerItens>
       </C.Container>
