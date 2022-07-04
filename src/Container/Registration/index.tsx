a899badd1949d5717cdeaf0987f6353136691f25
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-07-04 15:43:17 -0300

    starting registration page

---
+++ src/Container/Registration/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as C from './style'
+
+function Registration(){
+    return(
+        <C.Container>
+            Ola
+        </C.Container>
+    )
+}
+
+export default Registration;
