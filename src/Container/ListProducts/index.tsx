2f23d91603c0c798166313d9a3c5863def13b381
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-07-06 17:21:20 -0300

    I created the functions that open and close the modal and passed one to the bookmarks button in the header

--- src/Container/ListProducts/index.tsx
+++ src/Container/ListProducts/index.tsx
@@ -7,6 +7,7 @@ import { deleteItem, listProducst as listProducst_ } from '../../Services/Produc
 import Modal from 'react-modal'
 Modal.setAppElement('#root')
 
+
 export function ListProducts() {
     const [allProductsList, setAllProductsList] = useState<Array<Product>>([])
     const [nonFavoriteProductsList, setNonFavoriteProductsList] = useState<Array<Product>>([])
@@ -19,11 +20,11 @@ export function ListProducts() {
         let products = await listProducst_()
         setAllProductsList(products)
     }
-    // ESPECIFICAR OQUE FAZ
+    //start the page with the list of products
     useEffect(() => {
         getProducts()
     }, [])
-    //start the page with the list of products
+    //"shows the list updated every time we remove or add an object to the list (favorite or not)"
     useEffect(() => {
         parseFavoriteProducts()
     }, [allProductsList])
@@ -35,7 +36,7 @@ export function ListProducts() {
             console.log(error)
         }
     }
-    //"function that sends the product id to the registration page"
+    //function that sends the product id to the registration page
     const handleproductUpdate = async (id: any) => {
         try {
             history.push(`/products?id=${id}`)
@@ -43,7 +44,7 @@ export function ListProducts() {
             console.log(error)
         }
     }
-    // ESPECIFICAR OQUE FAZ
+    //Add products to favorites list
     const handlecheckStar = async (id: string) => {
         let favoriteIds: string[] = []
         let oldFavoriteIds = localStorage.getItem("favoriteIds")
@@ -58,7 +59,7 @@ export function ListProducts() {
         }
         parseFavoriteProducts()
     }
-    // ESPECIFICAR OQUE FAZ 
+    // remove products from favorites list
     const handlerUnCheckStar = async (id: string) => {
         let NofavoriteIds: string[] = []
         let FavoriteIds = localStorage.getItem("favoriteIds")
@@ -73,7 +74,7 @@ export function ListProducts() {
         }
         // should not add
     }
-    // ESPECIFICAR OQUE FAZ
+    // separates favorite from unfavorable products and saves in state variables
     const parseFavoriteProducts = async () => {
         let favoriteIdsString = localStorage.getItem("favoriteIds")
         let favoriteProducts: Product[] = []
@@ -97,19 +98,21 @@ export function ListProducts() {
         setfavoriteProductsList(favoriteProducts)
         setNonFavoriteProductsList(nonFavoriteProducts)
     }
-      //function that opens the modal
-      const handleOpenNewTransactionModal=()=>{
+    //function that opens the modal
+    const handleOpenNewTransactionModal=()=>{
         setIsNewTransactionModalOpen(true)
        }
        //function that closes the modal
        const handleCloseNewTransactionModal=()=>{
         setIsNewTransactionModalOpen(false)
        }
+
+     
     return (
         <>
             <C.Container>
                 <C.ContainerItens>
-                    <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+                <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
                     <h1>Lista</h1>
                     <div className='List'>
                         <table >
@@ -145,49 +148,7 @@ export function ListProducts() {
                             </tbody>
                         </table>
                     </div>
-                    <Modal isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                    overlayClassName="react-modal-overlay"
-                    className="react-modal-content"
-                    >
-                    <button className='react-modal-close' 
-                    onClick={handleCloseNewTransactionModal}><i className="fa-solid fa-x"></i>
-                    </button>
-                    <div className='Favorite'>
-                    <h1>Favoritos</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>SKU</th>
-                                    <th>Valor</th>
-                                    <th>Criado em</th>
-                                    <th>Atualizado em</th>
-                                    <th>Editado por</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {favoriteProductsList.map((product) => (
-                                    <tr key={product.id}>
-                                        <td>{product.id}</td>
-                                        <td className='ProductName'>{product.name}</td>
-                                        <td>{product.SKU}</td>
-                                        <td>{product.value}</td>
-                                        <td>{product.createdAt}</td>
-                                        <td>{product.updatedAt}</td>
-                                        <td>{product.updatedBy}</td>
-                                        <td>
-                                            <button onClick={() => handleproductUpdate(product.id)}><i className="fa-solid fa-pen-to-square"></i></button>
-                                            <button onClick={() => handleDelete(product.id)}><i className="fa-solid fa-trash"></i></button>
-                                            <button onClick={() => handlerUnCheckStar(product.id!)}><i className="fa-solid fa-star"></i></button>
-                                        </td>
-                                    </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    </Modal>
+                   
                 </C.ContainerItens>
             </C.Container>
         </>
