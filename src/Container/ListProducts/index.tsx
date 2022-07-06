f0b6b87b20a6385ba894f65f15aaf2333d2d4af2
Gabriel Lourenço <lourencogabriel77@gmail.com>
2022-07-06 14:17:42 -0300

    function that lists products and function that deletes by id created

--- src/Container/ListProducts/index.tsx
+++ src/Container/ListProducts/index.tsx
@@ -1,10 +1,32 @@
+import react, { useState, useEffect } from 'react';
 import { Header } from '../../Components/Header';
+import { useHistory } from 'react-router-dom';
 import * as C from './style'
+import { Product } from '../../Interface/Product';
+import { deleteItem, listProducst as listProducst_ } from '../../Services/Products';
 
 export function ListProducts() {
+    const [allProductsList, setAllProductsList] = useState<Array<Product>>([])
+    const history = useHistory()
 
+    //function that lists the products
+    const getProducts = async () =>{
+        let products = await listProducst_()
+        setAllProductsList(products)
+    }
+    //start the page with the list of products
+    useEffect(()=>{
+        getProducts()
+    },[allProductsList])
 
-
+    //function that deletes object
+    const handleDelete = async(id:any)=>{
+        try {
+            await deleteItem(id)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <>
             <C.Container>
@@ -26,23 +48,26 @@ export function ListProducts() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>3</td>
-                                    <td className='ProductName'>celular</td>
-                                    <td>1313</td>
-                                    <td>18,00</td>
-                                    <td>12/02/2020</td>
-                                    <td>12/02/2020</td>
-                                    <td>Gabriel</td>
+                                {allProductsList.map((product)=>(
+                                    <tr key={product.id}>
+                                    <td>{product.id}</td>
+                                    <td className='ProductName'>{product.name}</td>
+                                    <td>{product.SKU}</td>
+                                    <td>{product.value}</td>
+                                    <td>{product.createdAt}</td>
+                                    <td>{product.updatedAt}</td>
+                                    <td>{product.updatedBy}</td>
                                     <td>
                                         <button ><i className="fa-solid fa-pen-to-square"></i></button>
-                                        <button ><i className="fa-solid fa-trash"></i></button>
+                                        <button onClick={()=>handleDelete(product.id)}><i className="fa-solid fa-trash"></i></button>
                                         <button ><i className="fa-regular fa-star"></i></button>
                                     </td>
                                 </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
+                    {/* //Colocar em um modal */}
                     <h1>Favoritos</h1>
                     <div className='Favorite'>
                     <table>
